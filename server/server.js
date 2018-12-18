3b8250d508ad9c1dd44f628a10d456f6135a9790
lcom131-one <akshitapatel522@gmail.com>
2018-12-18 18:23:12 +0530

    sds

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
+const http=require('http');
 
 const path=require('path');
-
+const socketIO=require('socket.io');
 const express=require('express');
 const publicPath=path.join(__dirname,'../public'); 
 var port=process.env.PORT || 5000;
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath))
-
-app.listen(port,()=>
+io.on("connection",(socket)=>
+{
+    console.log("new User connected");
+    
+    socket.emit('newMessage',{
+        "from":"shahid@gmail.com",
+        "text":"hello", 
+        createdAt:123
+    });
+    socket.on('createMessage',(message)=>
+    {
+        console.log("Create Message",message);
+    });
+    
+    socket.on("disconnect",(socket)=>
+          {
+            console.log("User disconnected");
+        });    
+});
+server.listen(port,()=>
           {
    console.log(`Server Connected ${port}`); 
 });
