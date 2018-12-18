60d779e0ab2637a1f5a65788aaa4ebb78db7246b
lcom131-one <akshitapatel522@gmail.com>
2018-12-18 17:06:19 +0530

    first

---
+++ server/server.js
@@ -0,0 +1,13 @@
+
+const path=require('path');
+
+const express=require('express');
+const publicPath=path.join(__dirname,'../public'); 
+var port=process.env.PORT || 5000;
+var app=express();
+app.use(express.static(publicPath))
+
+app.listen(port,()=>
+          {
+   console.log(`Server Connected ${port}`); 
+});
